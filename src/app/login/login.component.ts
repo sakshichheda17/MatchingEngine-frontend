76816056c9b0be0245c5851b153732ada598f52e
csakshi17 <sakshichheda178@gmail.com>
2020-06-16 17:13:10 +0530

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  username : string;
+  password : string;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+    if ( this.username == "user1" && this.password == '1234'){
+      console.log("success");
+      this.router.navigate(['../', 'dashboard']);
+    }
+    else if ( this.username == "user2" && this.password == '5678'){
+      console.log("success");
+      this.router.navigate(['../', 'dashboard']);
+    }
+    else{
+      alert("Invalid credentials");
+    }
+  }
+}
